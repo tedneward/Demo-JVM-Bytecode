883c8eaefef983bf1e6410549cc32a12d55aab6e
Ted Neward <ted@tedneward.com>
2025-04-13 01:20:35 -0700

    More demos prep

---
+++ JavaExamples/app/src/main/java/com/newardassociates/demo/StringConcat.java
@@ -0,0 +1,10 @@
+package com.newardassociates.demo;
+
+public class StringConcat {
+    public static String concatThis() {
+        int x = 5;
+        int y = 7;
+
+        return "(" + x + "," + y + ")";
+    }
+}
--- jasm/src/test/java/com/newardassociates/demo/InstructionDemosTests.java
+++ jasm/src/test/java/com/newardassociates/demo/InstructionDemosTests.java
@@ -6,9 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class InstructionDemosTests {
     @Test void testSimpleIntReturn() { assertThat(InstructionDemos.simpleIntReturn()).isEqualTo(5); }
-
     @Test void testConstantReturn() { assertThat(InstructionDemos.simpleConstantReturn()).isEqualTo(432); }
-
     /* Just don't crash! */
     @Test void testStackFun() { InstructionDemos.stackFun(); }
 
