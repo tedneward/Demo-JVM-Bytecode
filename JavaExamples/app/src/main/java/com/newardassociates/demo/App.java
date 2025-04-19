9bcf70c421f65898c96b5efdcb25be8b90bb0b5f
Ted Neward <ted@tedneward.com>
2025-04-18 22:03:16 -0700

    Gotta use the right marker syntax

--- JavaExamples/app/src/main/java/com/newardassociates/demo/App.java
+++ JavaExamples/app/src/main/java/com/newardassociates/demo/App.java
@@ -4,9 +4,9 @@
 package com.newardassociates.demo;
 
 public class App {
-    // ((## BEGIN hello-world ##))
+    // {{## BEGIN hello-world ##}}
     public static void main(String[] args) {
         System.out.println(new Greeter().getGreeting());
     }
-    // ((## END hello-world ##))
+    // {{## END hello-world ##}}
 }
